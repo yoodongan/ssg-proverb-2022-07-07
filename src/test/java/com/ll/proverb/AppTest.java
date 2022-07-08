dedbd9d8d0a6b21db27f80394d9fd7f4827ed4d6
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-07-08 14:35:51 +0900

    Test 추가

--- src/test/java/com/ll/proverb/AppTest.java
+++ src/test/java/com/ll/proverb/AppTest.java
@@ -1,2 +1,37 @@
-package com.ll.proverb;public class AppTest {
+package com.ll.proverb;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void 테스트() {
+        int rs = 10 + 20;
+        assertEquals(30, rs);
+    }
+
+    @Test
+    public void 테스트_스캐너() {
+        String input = """
+                등록
+                명언1
+                작가1
+                """.stripIndent();
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        Scanner sc = new Scanner(in);
+
+        String cmd = sc.nextLine().trim();
+        String content = sc.nextLine().trim();
+        String author = sc.nextLine().trim();
+
+        assertEquals("등록", cmd);
+        assertEquals("명언1", content);
+        assertEquals("작가1", author);
+    }
+
 }
