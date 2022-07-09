5f4c8bd9f594cc2ea8309ee35d656671935107dd
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-07-09 23:19:08 +0900

    객체가 등록되도록 수정, 목록 기능 구현

--- src/main/java/com/ll/proverb/App.java
+++ src/main/java/com/ll/proverb/App.java
@@ -6,6 +6,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -22,9 +24,10 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         // 가장 마지막 명언글의 번호
+        List<WiseSaying> wiseSayings = new ArrayList<>();    // WiseSayings 객체를 리스트 형태로 저장.
         int wiseSayingLastId = 0;
 
-        JSONObject json = new JSONObject();
+
         outer:
         while (true) {
             System.out.printf("명령) ");
@@ -39,9 +42,17 @@ public class App {
                     int id = ++wiseSayingLastId; // 명언 글 번호 증가
 
                     WiseSaying wiseSaying = new WiseSaying(id, content, author);
-                    System.out.println(wiseSaying);
+                    wiseSayings.add(wiseSaying);
 
                     System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    break;
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("-------------------");
+                    for (int i = wiseSayings.size() - 1; i >= 0; i--) {    // 큰수부터 내림차순으로 출력한다.
+                        WiseSaying wiseSaying_ = wiseSayings.get(i);  // List에서 WiseSaying 객체 빼내오기.
+                        System.out.printf("%d / %s / %s\n", wiseSaying_.id, wiseSaying_.content, wiseSaying_.author);
+                    }
                     break;
                 case "종료":
                     break outer;
