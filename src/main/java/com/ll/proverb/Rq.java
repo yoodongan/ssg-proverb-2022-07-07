eea8391cf3f32039c1ab65afc5ee3fb177ca225e
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-07-11 22:20:52 +0900

    refactoring : path와 queryStr 추가해서 중복 제거하기

--- src/main/java/com/ll/proverb/Rq.java
+++ src/main/java/com/ll/proverb/Rq.java
@@ -2,20 +2,26 @@ package com.ll.proverb;
 
 public class Rq {
     public String url;
+    public String path;
+    public String queryStr;
+
     public Rq(String url) {
         this.url = url;
-    }
-
-    public int getIntParam(String paramName, int defaultValue) {
         String[] urlBits = url.split("\\?", 2);
+        this.path = urlBits[0];   // '삭제'와 같이 명령 부분만 저장.
+        if (urlBits.length == 2) {
+            this.queryStr = urlBits[1];   // url의 ? 뒤 나머지 부분을 queryStr 이라고 한다. 삭제?id=1&no=3
+        }
 
-        if (urlBits.length == 1) {    // 삭제? 만 입력한 경우.
+    }
+    public int getIntParam(String paramName, int defaultValue) {
+        if (queryStr == null) {
             return defaultValue;
         }
 
-        urlBits = urlBits[1].split("&");
+        String[] bits = queryStr.split("&");
 
-        for(String urlBit : urlBits) {
+        for(String urlBit : bits) {
             String[] paramNameAndValue = urlBit.split("=", 2);
             String paramName_ = paramNameAndValue[0];
             String paramValue = paramNameAndValue[1];
@@ -28,7 +34,6 @@ public class Rq {
     }
 
     public String getPath() {
-        String[] urlBits = url.split("\\?",2);
-        return urlBits[0];
+        return path;
     }
 }
